a70806dbc10dd657666eed6744df28e5d36d9c95
Anilabh Barua <anilabh.barua@gmail.com>
2025-05-12 15:30:18 +0530

    #0090

--- Assignment/lex.yy.c
+++ Assignment/lex.yy.c
@@ -282,32 +282,30 @@ static void yy_fatal_error YY_PROTO(( yyconst char msg[] ));
 	*yy_cp = '\0'; \
 	yy_c_buf_p = yy_cp;
 
-#define YY_NUM_RULES 11
-#define YY_END_OF_BUFFER 12
-static yyconst short int yy_accept[33] =
+#define YY_NUM_RULES 5
+#define YY_END_OF_BUFFER 6
+static yyconst short int yy_accept[12] =
     {   0,
-        0,    0,   12,   10,    9,    9,    8,    7,    6,    5,
-        5,    5,    5,    5,    9,    6,    5,    5,    5,    5,
-        5,    5,    5,    2,    5,    1,    5,    5,    3,    5,
-        4,    0
+        0,    0,    6,    4,    2,    3,    2,    0,    0,    1,
+        0
     } ;
 
 static yyconst int yy_ec[256] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    2,    3,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    2,    1,    1,    1,    1,    1,    1,    1,    4,
-        4,    5,    5,    4,    5,    1,    5,    6,    6,    6,
-        6,    6,    6,    6,    6,    6,    6,    1,    4,    5,
-        5,    5,    1,    1,    7,    7,    7,    7,    8,    7,
-        7,    7,    9,    7,    7,    7,    7,    7,    7,    7,
-        7,    7,    7,   10,    7,    7,    7,   11,    7,    7,
-        1,    1,    1,    1,    7,    1,   12,    7,    7,    7,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    3,    4,    4,    4,    5,    4,
+        4,    4,    6,    4,    4,    4,    4,    4,    3,    4,
+        4,    4,    4,    7,    3,    4,    4,    8,    4,    4,
+        1,    1,    1,    1,    1,    1,    3,    4,    4,    4,
 
-       13,   14,    7,    7,   15,    7,    7,   16,    7,   17,
-       18,    7,    7,   19,    7,   20,   21,    7,    7,    7,
-        7,    7,    4,    1,    4,    1,    1,    1,    1,    1,
+        3,    4,    4,    4,    3,    4,    4,    4,    4,    4,
+        3,    4,    4,    4,    4,    4,    3,    4,    4,    4,
+        4,    4,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -324,49 +322,35 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static yyconst int yy_meta[22] =
+static yyconst int yy_meta[9] =
     {   0,
-        1,    1,    1,    1,    1,    2,    2,    2,    2,    2,
-        2,    2,    2,    2,    2,    2,    2,    2,    2,    2,
-        2
+        1,    1,    1,    1,    1,    1,    1,    1
     } ;
 
-static yyconst short int yy_base[34] =
+static yyconst short int yy_base[12] =
     {   0,
-        0,    0,   45,   46,   20,   22,   46,   46,   38,    0,
-       32,   26,   24,   27,   24,   33,    0,   29,   19,   16,
-       15,   24,   21,    0,   11,    0,   11,   11,    0,   12,
-        0,   46,   26
+        0,    0,   12,   13,   13,   13,    3,    4,    2,   13,
+       13
     } ;
 
-static yyconst short int yy_def[34] =
+static yyconst short int yy_def[12] =
     {   0,
-       32,    1,   32,   32,   32,   32,   32,   32,   32,   33,
-       33,   33,   33,   33,   32,   32,   33,   33,   33,   33,
-       33,   33,   33,   33,   33,   33,   33,   33,   33,   33,
-       33,    0,   32
+       11,    1,   11,   11,   11,   11,   11,   11,   11,   11,
+        0
     } ;
 
-static yyconst short int yy_nxt[68] =
+static yyconst short int yy_nxt[22] =
     {   0,
-        4,    5,    6,    7,    8,    9,   10,   11,   10,   10,
-       10,   10,   10,   12,   13,   10,   10,   10,   14,   10,
-       10,   15,   15,   15,   15,   15,   15,   17,   31,   30,
-       29,   28,   27,   26,   25,   24,   23,   22,   16,   21,
-       20,   19,   18,   16,   32,    3,   32,   32,   32,   32,
-       32,   32,   32,   32,   32,   32,   32,   32,   32,   32,
-       32,   32,   32,   32,   32,   32,   32
+        4,    4,    5,    6,    7,    5,    6,    6,   10,    9,
+        8,   11,    3,   11,   11,   11,   11,   11,   11,   11,
+       11
     } ;
 
-static yyconst short int yy_chk[68] =
+static yyconst short int yy_chk[22] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    5,    5,    6,    6,   15,   15,   33,   30,   28,
-       27,   25,   23,   22,   21,   20,   19,   18,   16,   14,
-       13,   12,   11,    9,    3,   32,   32,   32,   32,   32,
-       32,   32,   32,   32,   32,   32,   32,   32,   32,   32,
-       32,   32,   32,   32,   32,   32,   32
+        1,    1,    1,    1,    1,    1,    1,    1,    9,    8,
+        7,    3,   11,   11,   11,   11,   11,   11,   11,   11,
+       11
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -380,12 +364,13 @@ static char *yy_last_accepting_cpos;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "token_count.l"
+#line 1 ".\\vowel_consonant.l"
 #define INITIAL 0
-#line 2 "token_count.l"
+#line 2 ".\\vowel_consonant.l"
 #include <stdio.h>
-int token_count = 0;
-#line 389 "lex.yy.c"
+int vowel_count = 0;
+int consonant_count = 0;
+#line 374 "lex.yy.c"
 
 /* Macros after this point can all be overridden by user definitions in
  * section 1.
@@ -536,10 +521,10 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
 
-#line 6 "token_count.l"
+#line 7 ".\\vowel_consonant.l"
 
 
-#line 543 "lex.yy.c"
+#line 528 "lex.yy.c"
 
 	if ( yy_init )
 		{
@@ -590,13 +575,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 33 )
+				if ( yy_current_state >= 12 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 46 );
+		while ( yy_base[yy_current_state] != 13 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -624,60 +609,36 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 8 "token_count.l"
+#line 9 ".\\vowel_consonant.l"
 { return 0; }
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 10 "token_count.l"
-{ printf("Keyword: %s\n", yytext); token_count++; }
+#line 11 ".\\vowel_consonant.l"
+{ 
+    printf("Vowel: %s\n", yytext);
+    vowel_count++;
+}
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 11 "token_count.l"
-{ printf("Keyword: %s\n", yytext); token_count++; }
+#line 16 ".\\vowel_consonant.l"
+{
+    printf("Consonant: %s\n", yytext);
+    consonant_count++;
+}
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 12 "token_count.l"
-{ printf("Keyword: %s\n", yytext); token_count++; }
+#line 21 ".\\vowel_consonant.l"
+{  }
 	YY_BREAK
 case 5:
 YY_RULE_SETUP
-#line 14 "token_count.l"
-{ printf("Identifier: %s\n", yytext); token_count++; }
-	YY_BREAK
-case 6:
-YY_RULE_SETUP
-#line 16 "token_count.l"
-{ printf("Number: %s\n", yytext); token_count++; }
-	YY_BREAK
-case 7:
-YY_RULE_SETUP
-#line 18 "token_count.l"
-{ printf("Operator: %s\n", yytext); token_count++; }
-	YY_BREAK
-case 8:
-YY_RULE_SETUP
-#line 20 "token_count.l"
-{ printf("Punctuation: %s\n", yytext); token_count++; }
-	YY_BREAK
-case 9:
-YY_RULE_SETUP
-#line 22 "token_count.l"
-{ /* skip whitespace */ }
-	YY_BREAK
-case 10:
-YY_RULE_SETUP
-#line 24 "token_count.l"
-{ printf("Unknown token: %s\n", yytext); token_count++; }
-	YY_BREAK
-case 11:
-YY_RULE_SETUP
-#line 26 "token_count.l"
+#line 23 ".\\vowel_consonant.l"
 ECHO;
 	YY_BREAK
-#line 681 "lex.yy.c"
+#line 642 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -969,7 +930,7 @@ static yy_state_type yy_get_previous_state()
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 33 )
+			if ( yy_current_state >= 12 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -1004,11 +965,11 @@ yy_state_type yy_current_state;
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 33 )
+		if ( yy_current_state >= 12 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 32);
+	yy_is_jam = (yy_current_state == 11);
 
 	return yy_is_jam ? 0 : yy_current_state;
 	}
@@ -1563,7 +1524,7 @@ int main()
 	return 0;
 	}
 #endif
-#line 26 "token_count.l"
+#line 23 ".\\vowel_consonant.l"
 
 
 int yywrap() {
@@ -1571,8 +1532,9 @@ int yywrap() {
 }
 
 int main() {
-    printf("Enter code or text (type EXIT to finish):\n");
+    printf("Enter text (type EXIT to finish):\n");
     yylex();
-    printf("Total number of tokens: %d\n", token_count);
+    printf("\nTotal Vowels: %d\n", vowel_count);
+    printf("Total Consonants: %d\n", consonant_count);
     return 0;
 }
